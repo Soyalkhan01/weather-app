b19855938534b0d91232b91b3d581c79516baa89
shaheerkhan <its.shaheerkhan728@gmail.com>
2026-05-18 21:39:44 +0530

    updated backend url

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -12,7 +12,7 @@ const App = () => {
   const [loading, setLoading] = useState(false);
 
 
-  const BACKEND_URL = "http://127.0.0.1:5000";
+const BACKEND_URL = "https://weather-backend.onrender.com";
   const getWeather = async (selectedCity) => {
 
     if (!selectedCity) return;
