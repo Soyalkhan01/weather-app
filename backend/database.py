c09c8327c4672b4b0afc3fc76498fdd270ca31d5
shaheerkhan <its.shaheerkhan728@gmail.com>
2026-05-22 07:05:11 +0530

    feat: add delete_city function to handle file-based history deletion

--- backend/database.py
+++ backend/database.py
@@ -4,31 +4,29 @@ import os
 FILE = "history.json"
 
 if not os.path.exists(FILE):
-
     with open(FILE, "w") as f:
         json.dump([], f)
 
 def save_city(city):
-
     with open(FILE, "r") as f:
         data = json.load(f)
 
-    data = [
-
-        item for item in data
-
-        if item["city"].lower() != city.lower()
-
-    ]
-
-    data.insert(0, {
-        "city": city
-    })
+    data = [item for item in data if item["city"].lower() != city.lower()]
+    data.insert(0, {"city": city})
 
     with open(FILE, "w") as f:
         json.dump(data, f, indent=4)
 
 def get_history():
-
     with open(FILE, "r") as f:
         return json.load(f)
+
+def delete_city(city):
+    with open(FILE, "r") as f:
+        data = json.load(f)
+
+    data = [item for item in data if item["city"].lower() != city.lower()]
+
+    with open(FILE, "w") as f:
+        json.dump(data, f, indent=4)
+    return True
